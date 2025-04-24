989a5a967637bf91eb8c5202955cf47c4e4e4f7c
linqingyang <2829004293@qq.com>
2025-04-24 19:35:32 +0800

    init

--- src/entrypoint.cc
+++ src/entrypoint.cc
@@ -1,17 +1,9 @@
 #include "entrypoint.hh"
+// #include "main.h"
 
-#include "hal/base/gpio.hh"
-#include "hal/base/timer.hh"
+#include <stdio.h>
+
+// #include "stm32f4xx_hal.h"
 
 void entrypoint() {
-    using namespace hal;
-
-    gpio::PH<10> blue {};
-    gpio::PH<11> green {};
-    gpio::PH<12> red {};
-
-    while (true) {
-        time::delay(1000);
-        gpio::toggle(green, blue, red);
-    }
 }
